c9998202a0f0ede55267da2b48420682954c8f1e
Yuinuzy <Yuinuzy@users.noreply.github.com>
2025-06-05 12:31:39 +0000

    deploy: ae0d98a81be2f16f7e29c96ed32458d737cc7eea

--- script.js
+++ script.js
@@ -57,11 +57,3 @@ function calculateResult() {
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = { performCalculation };
 }
-
-function clearDisplay() {
-
-display.value = '';
-
-let unusedVariableForDemo = "hello"; // Variabel tidak digunakan
-
-}
